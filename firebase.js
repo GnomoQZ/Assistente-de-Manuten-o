0eafaeaceb8c6c90c9cb58cac02c2f722466aa76
GnomoQZ <202402914677@alunos.estacio.br>
2026-02-15 21:34:22 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCU34TdZmsOw6T8MX36jcLoqW-qgOKPzik",
+  authDomain: "assistentemanutencaocafe.firebaseapp.com",
+  projectId: "assistentemanutencaocafe"
+};
+
+const app = initializeApp(firebaseConfig);
+
+alert("Firebase conectado com sucesso!");
